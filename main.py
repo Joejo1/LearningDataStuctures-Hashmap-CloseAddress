d4984405fd5cbc2fc0ffbfcbe4de4e95e56e4ff6
Joejo1 <thetester259@gmail.com>
2025-08-24 15:22:32 -0400

    added name file

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ emptyValue = '......'
 
 fl = np.array(pd.read_csv("name6000.csv"))
 numOfNames = len(fl)
-ar = np.array([[emptyValue] * 5 ] * (numOfNames * 2))
+ar = np.array([[emptyValue] * 5 ] * (numOfNames * 3))
 lenAr = len(ar)
 arColl = np.array([[emptyValue] * 2] * (numOfNames * 2))
-print(len(arColl))
 
 print('Number of Keys:', numOfNames)
 
@@ -27,6 +26,12 @@ def setKeyInAr(arr, index, key):
     arr[index][0] = key
     return arr
 
+def isSecondArKeyMatch(arr, index, key):
+    if arr[index][0] == key:
+        return True
+    else:
+        return False
+
 
 CollisionNum = 0
 numOfColl = 0
@@ -37,11 +42,12 @@ for i in fl:
     isColl = False
     isColl = isCollision(ar, index)
     offSet = -1
+    secondArMatching = False
 
     while isColl == True:
+        numOfColl = numOfColl + 1
         offSet = offSet + 1
         isColl = isCollision(arColl, offSet)
-        numOfColl = numOfColl + 1
     if offSet > -1:
         arColl = setKeyInAr(arColl, offSet, key)
         CollisionNum = CollisionNum + 1
@@ -59,9 +65,20 @@ for k in arColl:
         full = full + 1
 
     l = l + 1
+print('Collision populated:', full, 'Empty:', emty)
+emty = 0
+full = 0
+l = 0
+for k in ar:
+    if ar[l][0] == emptyValue:
+        emty = emty + 1
+    else:
+        full = full + 1
+
+    l = l + 1
 
 
-print('Populated:', full, 'Empty:', emty)
+print('Array populated:', full, 'Empty:', emty)
 
 print("num of coll:", numOfColl, "Number in second array:", CollisionNum)
 print(ar)
